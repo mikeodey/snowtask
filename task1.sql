96c7ba93e56b0344f5f82d6ca62a0ca521dba205
michael odey <michaelodey63@yahoo.com>
2024-04-30 23:02:44 +0100

    initial commit

---
+++ task1.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE TABLE GPT_TWEETS (
+ID varchar,
+Date varchar,
+Username varchar,
+Tweet varchar,
+ReplyCount int,
+RetweetCount int,
+LikeCount int,
+QuoteCount int,
+OnlyDate varchar,
+OnlyHour varchar,
+OnlyMin varchar,
+processed_tweet varchar,
+sentiment_label varchar,
+sentiment_score varchar);
+
